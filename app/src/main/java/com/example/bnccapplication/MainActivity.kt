89b344116c2733328ecaf7ca9c3d1c522db44953
Randi Adiel Gianufian <randiadiel30@gmail.com>
2020-09-03 20:44:14 +0700

    Create Intent for LookupActivity from MainActivity

--- app/src/main/java/com/example/bnccapplication/MainActivity.kt
+++ app/src/main/java/com/example/bnccapplication/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.example.bnccapplication
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btnLookup = findViewById<Button>(R.id.btn_lookup)
+        btnLookup.setOnClickListener {
+            openLookupActivity()
+        }
+    }
+    private fun openSecondActivity() {
+        val intent = Intent(this, SecondActivity::class.java).apply {
+            putExtra("Extras", "Hello SecondActivity from MainActivity")
+        }
+    }
+    private fun openLookupActivity() {
+        val intent = Intent(this,LookupActivity::class.java)
+        startActivity(intent)
+    }
+    private fun actionDial() {
+        val phoneNumber = "404558"
+        val intent = Intent().apply{
+            action = Intent.ACTION_DIAL
+            data = Uri.parse("tel:$phoneNumber")
+        }
+        startActivity(intent)
     }
 }
