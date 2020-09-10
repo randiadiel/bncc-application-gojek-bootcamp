0657eb8a27b6a05c50c277f39b1432356e3cb95e
Randi Adiel Gianufian <randiadiel30@gmail.com>
2020-09-11 01:11:21 +0700

    Create Bottom Sheet for hotline

--- app/src/main/java/com/example/bnccapplication/MainActivity.kt
+++ app/src/main/java/com/example/bnccapplication/MainActivity.kt
@@ -1,27 +1,62 @@
 package com.example.bnccapplication
 
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.Drawable
+import android.graphics.drawable.DrawableContainer
 import android.net.Uri
+import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.bnccapplication.hotline.HotlineData
+import com.example.bnccapplication.hotline.adapter.HotlineAdapter
 import com.example.bnccapplication.hotline.ui.HotlineActivity
 import com.example.bnccapplication.lookup.ui.LookupActivity
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import kotlinx.android.synthetic.main.activity_hotline.*
+import kotlinx.android.synthetic.main.activity_main.*
+import okhttp3.*
+import org.json.JSONArray
+import java.io.IOException
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
     companion object {
         const val extra = "Extra"
     }
+    private val okHttpClient = OkHttpClient()
+
+    private val mockHotlineList = mutableListOf<HotlineData>(
+        HotlineData("","","Loading...")
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val btnLookup = findViewById<Button>(R.id.btn_main_lookup)
         val btnHotline = findViewById<Button>(R.id.btn_main_hotline)
+        val btnHotlineCancel = findViewById<Button>(R.id.btn_main_hotline_cancel)
+        val hotlineAdapter = HotlineAdapter(mockHotlineList)
+        rv_main_hotline.layoutManager = LinearLayoutManager(this)
+        rv_main_hotline.adapter = hotlineAdapter
+
+        val request : Request = Request.Builder()
+            .url("https://bncc-corona-versus.firebaseio.com/v1/hotlines.json")
+            .build()
+
+        okHttpClient.newCall(request).enqueue(getCallback(hotlineAdapter))
         btnLookup.setOnClickListener {
             openLookupActivity()
         }
         btnHotline.setOnClickListener {
             openHotlineActivity()
+        }
+        btnHotlineCancel.setOnClickListener() {
+            closeBottomSheet()
         }
     }
     private fun openLookupActivity() {
@@ -31,10 +66,23 @@ class MainActivity : AppCompatActivity() {
         startActivity(intent)
     }
     private fun openHotlineActivity() {
-        val intent = Intent(this, HotlineActivity::class.java).apply {
-            putExtra(extra, "Message From MainActivity")
+//        val intent = Intent(this, HotlineActivity::class.java).apply {
+//            putExtra(extra, "Message From MainActivity")
+//        }
+//        startActivity(intent)
+        val bottomSheet = findViewById<View>(R.id.cl_bottom_sheet)
+        val bottomSheetBehavior : BottomSheetBehavior<View> = BottomSheetBehavior.from(bottomSheet)
+        val viewBg = findViewById<View>(R.id.bg)
+        bottomSheetBehavior.addBottomSheetCallback(getBottomSheetCallback())
+        if(bottomSheetBehavior.state == BottomSheetBehavior.STATE_COLLAPSED){
+            bottomSheetBehavior.state = BottomSheetBehavior.STATE_HALF_EXPANDED;
+            viewBg.visibility = View.VISIBLE
+        }else{
+            closeBottomSheet()
         }
-        startActivity(intent)
+        viewBg.setOnClickListener() {
+            closeBottomSheet()
+        }
     }
     private fun actionDial() {
         val phoneNumber = "404558"
@@ -43,5 +91,60 @@ class MainActivity : AppCompatActivity() {
             data = Uri.parse("tel:$phoneNumber")
         }
         startActivity(intent)
+    }
+    private fun closeBottomSheet(){
+        val bottomSheet = findViewById<View>(R.id.cl_bottom_sheet)
+        val bottomSheetBehavior : BottomSheetBehavior<View> = BottomSheetBehavior.from(bottomSheet)
+        val viewBg = findViewById<View>(R.id.bg)
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED;
+        viewBg.visibility = View.INVISIBLE
+    }
+    private fun getBottomSheetCallback() : BottomSheetBehavior.BottomSheetCallback {
+        val bottomSheet = findViewById<View>(R.id.cl_bottom_sheet)
+        return object : BottomSheetBehavior.BottomSheetCallback() {
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+            }
+
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+                if (slideOffset == 0f) closeBottomSheet()
+                else if (slideOffset == 1f) bottomSheet.background = ContextCompat.getDrawable(applicationContext, R.drawable.rectangle)
+                else bottomSheet.background = ContextCompat.getDrawable(applicationContext, R.drawable.layout_bg)
+            }
+        }
+    }
+    private fun getCallback(hotlineAdapter:HotlineAdapter) : Callback {
+        return object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                this@MainActivity.runOnUiThread {
+                    Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                try {
+                    val jsonString = response.body?.string()
+                    val jsonArray = JSONArray(jsonString)
+                    val hotlineListFromNetwork = mutableListOf<HotlineData>()
+
+                    for(i in 0 until jsonArray.length()) {
+                        hotlineListFromNetwork.add(
+                            HotlineData(
+                                imgIcon = jsonArray.getJSONObject(i).getString("img_icon"),
+                                name = jsonArray.getJSONObject(i).getString("name"),
+                                phone = jsonArray.getJSONObject(i).getString("phone")
+                            ))
+                    }
+                    this@MainActivity.runOnUiThread() {
+                        hotlineAdapter.updateData(hotlineListFromNetwork)
+                    }
+                } catch (e: Exception){
+                    this@MainActivity.runOnUiThread {
+                        Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+            }
+
+        }
     }
 }
